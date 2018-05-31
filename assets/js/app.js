46989f80585bc10ebd36c2666efda21988e78360
Jesus Marcano <user@MacBook-Pro-de-user.local>
2018-05-31 00:37:50 -0400

    Update

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,14 @@
-//alert("Hola Mundo")
+/*
+Author: Jesus Marcano
+Author URI: https://iapeto.com
+Description: CV
+Version: 1.0
+Tags: cv, curriculum vitae, hoja de vida
+*/
+
+var app = new Vue({
+  el: '#app',
+  data: {
+    message: 'Hello Vue!'
+  }
+})
